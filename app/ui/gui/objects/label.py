ca388b53e6e63734617c808a1e4eddab04a5c177
Artyom Davydov <x43872@gmail.com>
2022-06-04 06:12:07 +0300

    Center label text

--- app/ui/gui/objects/label.py
+++ app/ui/gui/objects/label.py
@@ -9,4 +9,4 @@ class Label(ttk.Label):
 
     def __init__(self, text, master, font_size=18, anchor=tk.CENTER, fg=FG, bg=BG):
         font = tkfont.Font(size=font_size)
-        super().__init__(master, text=text, foreground=fg, background=bg, font=font, anchor=anchor)
+        super().__init__(master, text=text, foreground=fg, background=bg, font=font, anchor=anchor, justify=tk. CENTER)
